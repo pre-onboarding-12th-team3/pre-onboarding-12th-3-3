37956144fb91b88594681293c744d45cf14a6dbf
ShinJaeIl01 <ekvm555@naver.com>
2023-09-07 22:04:35 +0900

    [Feat] 검색어 입력 시 커스텀훅 Fetch  호출 구현

--- src/components/domain/Form.tsx
+++ src/components/domain/Form.tsx
@@ -2,14 +2,20 @@ import { useState } from 'react';
 import { styled } from 'styled-components';
 
 import { Input, List } from '@/components';
+import useQuery from '@/hooks/useQuery';
 import { EventTypes } from '@/types/types';
 
 const Form = () => {
   const [searchText, setSearchText] = useState<string>('');
 
+  const { fetch } = useQuery();
+
   const changeSearchText = (event: EventTypes['changeInput']) => {
     const text = event.target.value;
     setSearchText(text);
+
+    // 디바운스 필요
+    if (text) fetch(text);
   };
 
   return (
