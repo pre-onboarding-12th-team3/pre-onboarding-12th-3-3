5dd737a6572026a2cdea5969058f7597991de9c5
ukssss <1872003115su@gmail.com>
2023-09-07 23:47:58 +0900

    [Feat] useDebounce 커스텀훅 제작

--- src/components/domain/Form.tsx
+++ src/components/domain/Form.tsx
@@ -1,16 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 
+import { getSearchData } from '@/apis/api';
 import { Input, List } from '@/components';
+import useDebounce from '@/hooks/useDebounce';
 import { EventTypes } from '@/types/types';
 
 const Form = () => {
   const [searchText, setSearchText] = useState<string>('');
+  const [searchResult, setSearchResult] = useState<string[]>([]);
+
+  // useDebounce 를 사용한 예시입니다. 로컬 캐싱부분이 생략된 단계라 수정하시면 됩니다. (14-33)
+  const debouncedSearchText = useDebounce(searchText, 400);
+
+  useEffect(() => {
+    if (debouncedSearchText) {
+      getSearchData(debouncedSearchText)
+        .then((res) => {
+          setSearchResult(res.data);
+        })
+        .catch((err) => {
+          console.error('Error : ', err);
+        });
+    } else {
+      setSearchResult([]);
+    }
+  }, [debouncedSearchText]);
 
   const changeSearchText = (event: EventTypes['changeInput']) => {
     const text = event.target.value;
     setSearchText(text);
   };
+
+  // 커밋 때문에 임시로 넣어둠 작업 하실 때 지우시면 됩니다.
+  console.log(searchResult);
 
   return (
     <Section>
